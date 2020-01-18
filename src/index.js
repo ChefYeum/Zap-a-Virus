97d38ef276d20710372a0f20f45a1acfca8d6c52
guljahonzohidova <gz4818@ic.ac.uk>
2020-01-18 20:53:25 +0000

    added enemies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as PIXI from "pixi.js-legacy";
 import { PixiConsole, PixiConsoleConfig } from "pixi-console";
 import rabbitImage from "./assets/rabbit.png";
+import wormImage from "./assets/worm.png";
+import cookieImage from "./assets/cookie.png";
+import horseImage from "./assets/trojan_horse.png";
 export class Main {
     constructor() {
         window.onload = () => {
@@ -10,6 +13,7 @@ export class Main {
     startLoadingAssets() {
         const loader = PIXI.Loader.shared;
         loader.add("rabbit", rabbitImage);
+        loader.add("worm", wormImage);
         loader.on("complete", () => {
             this.onAssetsLoaded();
         });
@@ -22,6 +26,7 @@ export class Main {
         const bunny = this.getBunny();
         bunny.position.set(Main.GAME_WIDTH / 2, Main.GAME_HEIGHT / 2);
         stage.addChild(bunny);
+        this.makeEnemies();
         this.app.ticker.add(() => {
             bunny.rotation += 0.05;
         });
@@ -47,10 +52,63 @@ export class Main {
         pixiConsole.show();
         this.app.stage.addChild(pixiConsole);
         console.log("Pixi-console added 🦾");
-        console.warn("Warnings example ✌");
-        setTimeout(() => {
-            throw new Error("Uncaught error example 👮‍♀️");
-        }, 0);
+        // console.warn("Warnings example ✌");
+        // setTimeout(() => {
+        //     throw new Error("Uncaught error example 👮‍♀️");
+        // }, 0);
+    }
+
+    makeEnemies() {
+        let numberOfBlobs = 6,
+        spacing = 48,
+        xOffset = 150,
+        speed = 2,
+        direction = 1;
+
+        //Make as many blobs as there are `numberOfBlobs`
+        for (let i = 0; i < numberOfBlobs; i++) {
+
+        let bunny = new PIXI.Sprite(PIXI.Texture.from("worm"));
+        let x = spacing * i + xOffset;
+        let y = Math.random() * (this.app.stage.height - bunny.height);
+
+        bunny.x = x;
+        bunny.y = y;
+        bunny.position.set(bunny.x, bunny.y);
+        // bunny.scale.set(2, 2);
+        this.app.stage.addChild(bunny);
+
+        // //Make a blob
+        // let blob = new Sprite(id["./assets/worm.png"]);
+
+        // //Space each blob horizontally according to the `spacing` value.
+        // //`xOffset` determines the point from the left of the screen
+        // //at which the first blob should be added
+        // let x = spacing * i + xOffset;
+
+        // //Give the blob a random `y` position
+        // let y = randomInt(0, stage.height - blob.height);
+
+        // //Set the blob's position
+        // blob.x = x;
+        // blob.y = y;
+
+        // //Set the blob's vertical velocity. `direction` will be either `1` or
+        // //`-1`. `1` means the enemy will move down and `-1` means the blob will
+        //  //move up. Multiplying `direction` by `speed` determines the blob's
+        // //vertical direction
+        // blob.vy = speed * direction;
+
+        // //Reverse the direction for the next blob
+        // direction *= -1;
+
+        //Push the blob into the `blobs` array
+        // blobs.push(blob);
+
+        //Add the blob to the `gameScene`
+        // stage.addChild(blob);
+    
+        }
     }
     getBunny() {
         const bunnyRotationPoint = {
